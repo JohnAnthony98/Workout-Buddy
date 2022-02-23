3167e6b0268181fb56e32a03dd115e3d03505a0f
JohnAnthony98 <janthony10@mail.csuchico.edu>
2022-02-23 02:04:58 -0800

    add text border for workouts

--- workout_buddy_app/lib/screens/workout_main.dart
+++ workout_buddy_app/lib/screens/workout_main.dart
@@ -3,42 +3,43 @@ import 'package:flutter/material.dart';
 class WorkoutMain extends StatefulWidget {
   WorkoutMain({Key? key, title}) : super(key: key);
 
+  @override
   _WorkoutMain createState() => _WorkoutMain();
 }
 
 class _WorkoutMain extends State<WorkoutMain> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 56, 53, 53),
       appBar: AppBar(
-        title: Text("Workout Page!"),
+        centerTitle: true,
+        title: const Text("Workouts!"),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have tapped the button this many times:',
+            Container(
+              margin: const EdgeInsets.all(15.0),
+              padding: const EdgeInsets.all(3.0),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blue, width: 5.0)),
+              child: const Text(
+                'Daily Workout Here',
+                style: TextStyle(color: Colors.white),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context,
+                    "/future_workouts"); ////////Pressing breaks app as of 2/22/2022////////
+              },
+              child: const Text("Future Workouts",
+                  style: TextStyle(fontSize: 26, color: Colors.white)),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
