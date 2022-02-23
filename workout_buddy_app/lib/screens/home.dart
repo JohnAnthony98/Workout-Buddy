db533fcd1092050d66f2887c6b76acb0b68332ee
JohnAnthony98 <janthony10@mail.csuchico.edu>
2022-02-23 01:37:01 -0800

    Added home page

---
+++ workout_buddy_app/lib/screens/home.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 56, 53, 53),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/workout_main");
+              },
+              child: const Text("Workout", style: TextStyle(fontSize: 26)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
