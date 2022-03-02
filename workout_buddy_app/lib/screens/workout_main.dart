b8fd3e5dfce14c33d86da5847a985b9dc047615c
JohnAnthony98 <janthony10@mail.csuchico.edu>
2022-03-02 08:23:40 -0800

    get workouts from database

--- workout_buddy_app/lib/screens/workout_main.dart
+++ workout_buddy_app/lib/screens/workout_main.dart
@@ -1,5 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:workout_buddy_app/services/my_colors.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class WorkoutMain extends StatefulWidget {
   WorkoutMain({Key? key, title}) : super(key: key);
@@ -10,6 +14,16 @@ class WorkoutMain extends StatefulWidget {
 
 class _WorkoutMain extends State<WorkoutMain> {
   DateTime viewingWorkoutDay = DateTime.now();
+  final User? user = FirebaseAuth.instance.currentUser;
+
+  Future<QuerySnapshot> getWorkout(String date, String? user_id) {
+    var returned_workout = FirebaseFirestore.instance
+        .collection('User_Workouts')
+        .where("UserID", isEqualTo: user_id)
+        .where("Date", isEqualTo: date)
+        .get();
+    return returned_workout;
+  }
 
   String getDate() {
     return viewingWorkoutDay.month.toString() +
@@ -48,20 +62,42 @@ class _WorkoutMain extends State<WorkoutMain> {
               padding: const EdgeInsets.all(3.0),
               decoration: BoxDecoration(
                   border: Border.all(color: getButtonColor(), width: 5.0)),
-              child: Text(
-                getDate(),
-                style: TextStyle(color: getButtonTextColor()),
+              child: FutureBuilder(
+                future: getWorkout(getDate(), user?.uid),
+                builder: (BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(
+                      child: SpinKitWave(
+                        color: getButtonTextColor(),
+                        size: 50.0,
+                      ),
+                    );
+                  } else {
+                    if (snapshot.data?.size != 0 && !snapshot.hasError) {
+                      return Text(
+                          "${getDate()}\n${snapshot.data?.docs.first['Workout']}",
+                          style: TextStyle(
+                              fontSize: 16, color: getButtonTextColor()));
+                    } else {
+                      return Text(
+                          "${getDate()}\nThere is no workout for Today!",
+                          style: TextStyle(
+                              fontSize: 16, color: getButtonTextColor()));
+                    }
+                  }
+                },
               ),
             ),
             ElevatedButton(
               onPressed: viewPreviousDay,
-              child: const Text("Previous Workout",
-                  style: TextStyle(fontSize: 26, color: Colors.white)),
+              child: Text("Previous Workout",
+                  style: TextStyle(fontSize: 26, color: getButtonTextColor())),
             ),
             ElevatedButton(
               onPressed: viewNextDay,
-              child: const Text("Next Workout",
-                  style: TextStyle(fontSize: 26, color: Colors.white)),
+              child: Text("Next Workout",
+                  style: TextStyle(fontSize: 26, color: getButtonTextColor())),
             ),
           ],
         ),
